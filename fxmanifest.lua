0ad689e8ce84ee41e672518df5f3e3ffcc417c2e
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-03-15 07:24:56 -0500

    Update fxmanifest.lua
    Spelling error 🤣

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'JoeV2'
-description 'A Script to convert prefix in chat to a emojie'
-version '1.0.0'
+description 'A Script to convert prefix in chat to a emoji'
+version '1.0.1'
 
 
 server_scripts {
